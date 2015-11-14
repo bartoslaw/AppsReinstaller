34ffba1c43455db1a774f8a5618cf887918da7ac
Bartol <bs.smektala@gmail.com>
2015-11-14 17:17:02 +0100

    apk downloader class

--- AppReinstaller/app/src/main/java/com/weareawesome/appreinstaller/helpers/ApkDownloader.java
+++ AppReinstaller/app/src/main/java/com/weareawesome/appreinstaller/helpers/ApkDownloader.java
@@ -1,11 +1,58 @@
 package com.weareawesome.appreinstaller.helpers;
 
+import android.content.Context;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.BinaryHttpResponseHandler;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import cz.msebera.android.httpclient.Header;
+
 public class ApkDownloader {
     public interface DownloadListener {
         void onDownloaded(String pathToApk);
     }
 
-    public void downloadApk(String url, DownloadListener listener) {
-        //todo invoke asynchronous download of the apk from given url then invoke listener.onDownloaded(path);
+    public void downloadApk(final Context cntx, final String url, final DownloadListener listener) {
+        if(listener == null)
+            throw new RuntimeException("Listener can't be null!");
+        if(url == null || url.isEmpty())
+            throw new RuntimeException("You need to specify url");
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(cntx, url, new BinaryHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] binaryData) {
+                //todo we should decide upon what we should name .apk files
+                listener.onDownloaded(saveFile(cntx, "filename", binaryData));
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] binaryData, Throwable error) {
+                //todo maybe we should retry?
+                Toast.makeText(cntx, "Download failed, please try again.", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private String saveFile(Context cntx, String fileName, byte[] rawData) {
+        FileOutputStream fileOutputStream;
+        try {
+            fileOutputStream = cntx.openFileOutput(fileName + ".apk", Context.MODE_PRIVATE);
+            fileOutputStream.write(rawData);
+
+            fileOutputStream.flush();
+            fileOutputStream.close();
+
+            //todo maybe we miss here some kind of null/error check?
+            return cntx.getFileStreamPath(fileName + ".apk").getAbsolutePath();
+        } catch(IOException e) {
+            Toast.makeText(cntx, "Writing file to storage failed.", Toast.LENGTH_LONG).show();
+        }
+
+        return "";
     }
 }
