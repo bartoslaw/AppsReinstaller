a3266a6bf860fa141bdacc502ab4f784b5a9df8b
Bartol <bs.smektala@gmail.com>
2015-11-14 17:31:36 +0100

    helper methods test stubs

---
+++ AppReinstaller/app/src/test/java/helpers/ApkDownloaderTest.java
@@ -0,0 +1,16 @@
+package helpers;
+
+import junit.framework.TestCase;
+
+public class ApkDownloaderTest extends TestCase {
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        //todo prepare apk downloader mock object
+    }
+
+    public void testDownloadApk() throws Exception {
+        //todo write test ;)
+    }
+}
---
+++ AppReinstaller/app/src/test/java/helpers/ApkInstallerTest.java
@@ -0,0 +1,22 @@
+package helpers;
+
+import junit.framework.TestCase;
+
+public class ApkInstallerTest extends TestCase {
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        //todo mock apk installer object
+    }
+
+    public void testInstall() throws Exception {
+        assertNotNull(new Object());
+        //todo write correct test ;)
+    }
+
+    public void testDeinstall() throws Exception {
+        assertNotNull(new Object());
+        //todo write correct test ;)
+    }
+}
